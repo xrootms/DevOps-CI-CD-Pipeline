7a449baa5cc17a1bd991f4675ffe03b410db08fc
saif25 <saif25@gmail.com>
2025-11-06 21:05:32 +0530

    created module security group for k8s

--- Infra-Setup-Terraform/main.tf
+++ Infra-Setup-Terraform/main.tf
@@ -7,3 +7,13 @@ module "networking" {
   cidr_private_subnet  = var.cidr_private_subnet
 
 }
+
+module "security_groups" {
+  source              = "./security-groups"
+  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  k8s_cluster_sg_name = "SG for k8s to enable 465, 30000-32767, 25, 3000-10000 & 6443"
+  vpc_id              = module.networking.devops_proj_1_vpc_id
+  # ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+  # ec2_nexus_sg_name   = "Allow port 8081 for nexus"
+  # ec2_sonar_sg_name   = "Allow port 9000 for sonar"
+}
